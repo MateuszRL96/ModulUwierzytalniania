822f5dcb816728b7e43195334c207f5315703dd3
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-11-02 19:05:24 +0100

    utworzenie serwisu jwt

---
+++ src/main/java/com/example/Auth/services/JwtService.java
@@ -0,0 +1,48 @@
+package com.example.Auth.services;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtService {
+    
+
+    public static final String SECRET ="D57850710EC3353E2175133A6A347E6615740739DF14E40E7F4C63C550854380";
+
+    public void validateToken(final String token)
+    {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey()
+    {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public String generateToken(String username)
+    {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+        .setClaims(claims)
+        .setSubject(username)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis()+ 1000 *60 *30))
+        .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+}
